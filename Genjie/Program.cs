bb28a74f5b29eb0a8758a8789a4753887c90ef04
Oliver Liljeblad <oliver.liljeblad@live.com>
2017-05-07 17:45:53 +0200

    fungerande program

--- Genjie/Program.cs
+++ Genjie/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Genjie
 {
@@ -8,8 +10,10 @@ namespace Genjie
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             var glosor = File.ReadAllText(@"Glosor\Spanska 10 maj.txt");
-            Console.WriteLine(glosor);
+            //Console.WriteLine(glosor);
 
             var lines = glosor.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
@@ -18,11 +22,40 @@ namespace Genjie
             foreach (var line in lines)
             {
                 var splitline = line.Split('=');
-                var svenska = splitline[0];
-                var spanska = splitline[1];
+                var svenska = splitline[0].Trim();
+                var spanska = splitline[1].Trim();
+
+                dict.Add(svenska, spanska);
             }
 
+            var rnd = new Random();
+
+
+            while (true)
+            {
+                var question = dict.Keys.ToArray()[rnd.Next(dict.Count)];
+                var correctAnswer = dict[question];
+
+                Console.WriteLine(question);
+                var answer = Console.ReadLine();
+
+                if (correctAnswer.ToLower() == answer.ToLower())
+                {
+                    Console.WriteLine("Rätt");
+                }
+                else
+                {
+                    Console.Write("Fel, rätt svar är:");
+                    Console.WriteLine(correctAnswer);
+                }
+
+                Console.WriteLine();
+            }
+
+
+
             Console.ReadLine();
+
         }
     }
 }
